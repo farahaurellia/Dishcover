fb3be2e6762882baee71e3abfd69678eb071eac8
farahaurellia <farahaurelliaa@gmail.com>
2024-12-03 21:57:47 +0700

    minor fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\HomepageController;
 
 
