1da73f6811645d85ffedbf26914d90cb0c62a6bd
farahaurellia <farahaurelliaa@gmail.com>
2024-12-05 22:23:06 +0700

    homepage changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DishCover Dashboard</title>
+        <title>DishCover</title>
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     </head>
