3dcb2de6c9fac77c14e74e8e3a974d17b97ea792
divaaaas <149087429+divaaaas@users.noreply.github.com>
2024-12-20 20:06:10 +0700

    show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,11 +9,11 @@
     <div class="container-view">
         <div class="container-content">
             <div class="left-side">
-                <a  onclick="window.history.back();" class="back-btn">
-                    <svg width="20" height="40" viewBox="0 0 20 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M5.57167 20.0001L17.3567 31.7851L15 34.1417L2.03667 21.1784C1.72422 20.8659 1.54869 20.442 1.54869 20.0001C1.54869 19.5581 1.72422 19.1343 2.03667 18.8217L15 5.8584L17.3567 8.21506L5.57167 20.0001Z" fill="black"/>
-                    </svg>
-                </a> 
+            <a onclick="window.history.back();" class="back-btn">
+                <svg width="20" height="40" viewBox="0 0 20 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.57167 20.0001L17.3567 31.7851L15 34.1417L2.03667 21.1784C1.72422 20.8659 1.54869 20.442 1.54869 20.0001C1.54869 19.5581 1.72422 19.1343 2.03667 18.8217L15 5.8584L17.3567 8.21506L5.57167 20.0001Z" fill="white"/>
+                </svg>
+            </a>
                 <div class="left-section">
                     <div class="img-wrapper">
                         <img src="{{ asset($recipe->image_url) }}" alt="Recipe Image">
